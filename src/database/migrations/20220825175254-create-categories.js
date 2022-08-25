453669a44d6c8b2a5468463a319ec50b1e5edf19
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 15:14:11 -0300

    feat [categories-migration]: creates up & down create & drop table

--- src/database/migrations/20220825175254-create-categories.js
+++ src/database/migrations/20220825175254-create-categories.js
@@ -2,20 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Categories', {
+      
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Categories');
   }
 };
