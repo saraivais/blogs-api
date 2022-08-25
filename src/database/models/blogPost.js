8965609f9568ef9b0317024e4abd4cbe2d8bc0ff
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 16:05:27 -0300

    feat [blogPost]: adds model keys & associate~

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -1,5 +1,27 @@
 const BlogPost = (sequelize, DataTypes) => {
-  const BlogPost = sequelize.define('BlogPost', {}, {});
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId', as: 'User'
+    });
+  };
 
   return BlogPost;
 };
