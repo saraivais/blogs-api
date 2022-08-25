929f6b1a2ddd340cc838c2c0ae433924e6f5551f
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 18:34:58 -0300

    feat [api]: imports and uses loginRoute & errorMiddleware~

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const loginRoute = require('./routes/login');
+const errorMiddleware = require('./errors/errorMiddleware');
 // ...
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/login', loginRoute);
 // ...
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
+app.use(errorMiddleware);
+
 module.exports = app;
