d6ea6c10e8da9c2b3728937febb41003a2583169
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 15:32:46 -0300

    feat [postscategories-migration]: creates postid & categoryid keys

--- src/database/migrations/20220825175323-create-post-categories.js
+++ src/database/migrations/20220825175323-create-post-categories.js
@@ -2,7 +2,28 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PostCategories', {});
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },        
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
