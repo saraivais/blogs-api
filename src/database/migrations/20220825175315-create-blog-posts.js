fa2c477bfec02ba26466885278f4aa7f0cde3964
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 15:19:44 -0300

    feat [blogposts-migration]: creates id, title & content keys~

--- src/database/migrations/20220825175315-create-blog-posts.js
+++ src/database/migrations/20220825175315-create-blog-posts.js
@@ -3,7 +3,20 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('BlogPosts', {
-      
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
     });
   },
 
