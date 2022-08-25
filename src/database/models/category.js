08b68b07c79807ba3677dd46834655a34791eab1
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 15:54:53 -0300

    feat [category]: adds model keys, options & returns model~ and fixes model name

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,6 +1,13 @@
 const Category = (sequelize, DataTypes) => {
-  const Category = sequelize.define('Categories', {
-    
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
   });
 };
 
