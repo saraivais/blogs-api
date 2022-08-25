c33e1536d7b8323e5acf0eb1467b5a17f3cb1d17
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 16:22:00 -0300

    feat [postCategory]: adds model keys & N:N associations~

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -1,5 +1,35 @@
 const PostCategory = (sequelize, DataTypes) => {
-  const PostCategory = sequelize.define('PostCategory', {}, {});
+  const PostCategory = sequelize.define('PostCategory',
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      }
+    },
+    {
+      timestamps: false,
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'Category',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'BlogPost',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  }
 
   return PostCategory;
 };
