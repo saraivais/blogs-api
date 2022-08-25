a92291d745d5d7637ccb9d404fc8b698a12db5e4
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 15:04:56 -0300

    feat [users-migration]: creates id key

--- src/database/migrations/20220825175158-create-users.js
+++ src/database/migrations/20220825175158-create-users.js
@@ -3,7 +3,24 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Users', {
-      
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      displayName: {
+
+      },
+      email: {
+
+      },
+      password: {
+
+      },
+      image: {
+
+      },
     });
   },
 
