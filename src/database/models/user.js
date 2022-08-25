7a25a7c3a984d624d4988ef349b82939492b0fdb
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 15:47:33 -0300

    feat [user]: adds model keys, options & returns model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,7 +1,19 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
   });
+
+  return User;
 };
 
 module.exports = User;
