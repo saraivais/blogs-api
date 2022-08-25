1b10cb1bd6c9c255350b03b23c00bcb281c7bb14
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 15:16:03 -0300

    feat [categories-migration]: creates id & name keys~

--- src/database/migrations/20220825175254-create-categories.js
+++ src/database/migrations/20220825175254-create-categories.js
@@ -3,7 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Categories', {
-      
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
     });
   },
 
