1c647ae67406a9fc5f93440948e7c4297580d230
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 19:49:50 -0300

    feat [userService]: creates exists function

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,10 @@
 const { User } = require('../database/models');
 
 const userService = {
+  exists: async (id) => {
+    const chosenUser = await User.findByPk(id);
+    return (chosenUser !== null);
+  },
 
 };
 
