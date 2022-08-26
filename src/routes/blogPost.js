ddedb769f6c7c85b60d0ed13663076dd3d0b03ce
saraivais <isadorasaraiva95@gmail.com>
2022-08-26 13:58:55 -0300

    feat [blogPostRoute]: adds getById endpoint

--- src/routes/blogPost.js
+++ src/routes/blogPost.js
@@ -6,5 +6,8 @@ const jwtValidation = require('../middlewares/auth');
 const blogPostRoute = express.Router();
 
 blogPostRoute.use(jwtValidation);
+blogPostRoute.get('/:id', rescue(blogPostController.getById));
+blogPostRoute.get('/', rescue(blogPostController.getAll));
+blogPostRoute.post('/', rescue(blogPostController.create));
 
 module.exports = blogPostRoute;
