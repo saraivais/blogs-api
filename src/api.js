39f33882508853a7d55c79f8aa942bb491ed8223
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 21:10:14 -0300

    feat [api]: imports & uses userRoute

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const loginRoute = require('./routes/login');
+const userRoute = require('./routes/user');
 const errorMiddleware = require('./errors/errorMiddleware');
 // ...
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/login', loginRoute);
+app.use('/user', userRoute);
 // ...
 
 // É importante exportar a constante `app`,
