8e8807f06ba7782f0c224a9edba30c7395e29144
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 22:14:31 -0300

    feat [categoryController]: creates getAll middleware~

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,7 +1,10 @@
 const categoryService = require('../services/category');
 
 const categoryController = {
-
+  getAll: async (_request, response, _next) => {
+    const allCategories = await categoryService.getAll();
+    return response.status(200).json(allCategories);
+  },
 };
 
 module.exports = categoryController;
