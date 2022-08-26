a880ddfee3e594865fed8baa3b15985ccf3e46ac
saraivais <isadorasaraiva95@gmail.com>
2022-08-26 11:48:24 -0300

    feat [blogPostsController]: implements create middleware

--- src/controllers/blogPost.js
+++ src/controllers/blogPost.js
@@ -1,7 +1,11 @@
 const blogPostService = require('../services/blogPost');
 
 const blogPostController = {
+  create: async (request, response, _next) => {
+    const createdPost = await blogPostService.create(request.body);
 
+    return response.status(201).json(createdPost);
+  },
 };
 
 module.exports = blogPostController;
