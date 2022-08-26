27608b6968a3e0793025fd88d34e44411ea787a0
saraivais <isadorasaraiva95@gmail.com>
2022-08-26 12:38:21 -0300

    feat [blogPostController]: adds getAll middleware~

--- src/controllers/blogPost.js
+++ src/controllers/blogPost.js
@@ -1,6 +1,12 @@
 const blogPostService = require('../services/blogPost');
 
 const blogPostController = {
+  getAll: async (_request, response, _next) => {
+    const allPosts = await blogPostService.getAll();
+
+    return response.status(200).json(allPosts);
+  },
+
   create: async (request, response, _next) => {
     const token = request.headers.authorization;
     const createdPost = await blogPostService.create(request.body, token);
