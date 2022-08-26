e08e005173705a9b957fad79345f1355d322cac9
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 21:09:23 -0300

    feat [userRoute]: implements endpoints~

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const rescue = require('express-rescue');
+const userController = require('../controllers/user');
+const jwtValidation = require('../middlewares/auth');
 
 const userRoute = express.Router();
+
+userRoute.get('/:id', jwtValidation, rescue(userController.getById));
+userRoute.get('/', jwtValidation, rescue(userController.getAll));
+userRoute.post('/', rescue(userController.create));
 
 module.exports = userRoute;
