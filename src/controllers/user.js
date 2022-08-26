7b51287fcffbd5455768bbba2652e49976f9aefa
saraivais <isadorasaraiva95@gmail.com>
2022-08-25 21:08:09 -0300

    feat [userController]: creates getAll middleware~

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,10 @@
-const userService = require("../services/user");
+const userService = require('../services/user');
 
 const userController = {
+  getAll: async (_request, response, _next) => {
+    const allUsers = await userService.getAll();
+    return response.status(200).json(allUsers);
+  },
 
 };
 
