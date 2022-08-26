8c46bd97265ba61fffa635de4c20f1724c686d7f
saraivais <isadorasaraiva95@gmail.com>
2022-08-26 11:30:05 -0300

    feat [blogPostService]: creates categoryId verification

--- src/services/blogPost.js
+++ src/services/blogPost.js
@@ -1,8 +1,10 @@
 const Joi = require('joi');
 const runSchema = require('./joiValidator');
 const { BlogPost } = require('../database/models');
+const categoryService = require('./category');
 
 const blogPostService = {
+
   validateBlogPostFiekds: runSchema(Joi.object({
     title: Joi.string().required().messages({
       'string.required': '400|Some required fields are missing',
@@ -14,6 +16,17 @@ const blogPostService = {
       'array.required': '400|"categoryIds" not found',
     }),
   })),
+
+  verifyAllCategories: async (categoryIds) => {
+    const allCategoriesVerified = await Promise.all(categoryIds
+      .map((categId) => categoryService.exists(categId)));
+    console.log('allCategoriesVerificed', allCategoriesVerified);
+    const allCategoriesValid = allCategoriesVerified.every((check) => check === true);
+    if (!allCategoriesValid) {
+      throw new Error('400|"categoryIds" not found');
+    }
+  },
+
 };
 
 module.exports = blogPostService;
