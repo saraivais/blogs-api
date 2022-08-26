f4f49c71e6c3278b1cc6d849bfd20d67cee4f83b
saraivais <isadorasaraiva95@gmail.com>
2022-08-26 12:20:02 -0300

    feat [blogPostModel]: adds default values for published & updated

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -11,8 +11,14 @@ const BlogPost = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       foreignKey: true,
     },
-    published: DataTypes.DATE,
-    updated: DataTypes.DATE,
+    published: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
   }, {
     timestamps: false,
   });
