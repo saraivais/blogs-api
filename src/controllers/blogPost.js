0ba0f44ce41a8fa66b82dab8ce5e7b9e6c1bd421
saraivais <isadorasaraiva95@gmail.com>
2022-08-26 12:19:32 -0300

    fix [blogPostController]: now sends token to create function~ to be able to generate userId

--- src/controllers/blogPost.js
+++ src/controllers/blogPost.js
@@ -2,7 +2,8 @@ const blogPostService = require('../services/blogPost');
 
 const blogPostController = {
   create: async (request, response, _next) => {
-    const createdPost = await blogPostService.create(request.body);
+    const token = request.headers.authorization;
+    const createdPost = await blogPostService.create(request.body, token);
 
     return response.status(201).json(createdPost);
   },
