25d33a3ff356341da894ffc044ff4e6224342e90
saraivais <isadorasaraiva95@gmail.com>
2022-08-26 11:50:33 -0300

    feat [api]: imports & uses blogPostRoute~

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const loginRoute = require('./routes/login');
 const userRoute = require('./routes/user');
 const categoryRoute = require('./routes/category');
+const blogPostRoute = require('./routes/blogPost');
 const errorMiddleware = require('./errors/errorMiddleware');
 // ...
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/login', loginRoute);
 app.use('/user', userRoute);
 app.use('/categories', categoryRoute);
+app.use('/post', blogPostRoute);
 // ...
 
 // É importante exportar a constante `app`,
